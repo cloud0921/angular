af7e14375384707f7430df90b2d80eceaa49cb99
SeoJaeHyuk <ssjjhhcc@gmail.com>
2023-05-23 20:14:27 +0900

    first commit

--- thirdStudy/src/app/app.module.ts
+++ thirdStudy/src/app/app.module.ts
@@ -1,29 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BoardComponent } from './board/board.component';
-
-import { RouterModule,Routes} from '@angular/router'; //라우터
-import { authGuard } from './auth.guard';
-import { FormsModule } from '@angular/forms';
-
-const router : Routes = [  //라우팅
-  {path : 'login' , component : LoginComponent},  
-  {path : 'board' , component : BoardComponent, canActivate:[authGuard]},  //가드 추가
-  {path : '', redirectTo : '/login',  pathMatch : 'full'}
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(router,{enableTracing:false}),
-    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
