b8f43ef9f6be25efa330e05860db406854643820
SeoJaeHyuk <ssjjhhcc@gmail.com>
2023-05-23 14:47:34 +0900

    commit

--- thirdStudy/src/app/ask.service.ts
+++ thirdStudy/src/app/ask.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AskService {
 
-  constructor() { }
+  //저장소(나중에 데이터베이스 서버)
+  private readonly storage = {
+    id : 'admin',
+    passwd : '1234'
+  }
+  constructor() { 
+
+  }
+
+  //로그인을 시도하는 함수
+  tryToLogin(param : any){
+    return new Observable( arg=>{  //관측대상 생성
+      if(param.id == this.storage.id && param.passwd == this.storage.passwd){  //로그인 정보가 맞다면
+        arg.next({status: true});  //성공
+        localStorage.setItem('status',"true");  //로컬저장소에 저장!
+      } else {
+        arg.next({status: false, reason : 'wrong information'});  //실패
+      }
+      arg.complete();
+    });
+  }
+  readonly isLogged: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  //로그인이 되었는지 확인하는 함수
+  isLogIn() : void{
+    if(localStorage.getItem('status') == 'true'){  //저장소에 로그인정보가 있는지 확인
+      this.isLogged.next(true);
+    } else {
+      this.isLogged.next(false);
+    }
+}
 }
--- thirdStudy/src/app/login/login.component.ts
+++ thirdStudy/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AskService } from '../ask.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   id : string="";
   passwd : string="";
-  constructor() { }
+  constructor(private service : AskService, private rout: Router) { 
+    //생성자에 private를 붙여서 의존성을 주입받으면 글로벌하게 사용가능 합니다.
+    //private가 있으므로 다른 컴포넌트나 html파일에서는 접근할 수 없습니다.
+  }
   ngOnInit(): void {
  
   }
 
   login() {
-    console.log(this.id, this.passwd);
+    this.service.tryToLogin({id : this.id, passwd : this.passwd}).subscribe( (arg:any)=>{
+      console.log(arg);
+      if(arg.status == true){
+        alert('로그인 성공!')
+        this.rout.navigate(['/board']);
+      }
+    });
   }
 }
