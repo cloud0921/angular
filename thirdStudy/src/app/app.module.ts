4682deb2a859401d5187f16206a5ba274fe7445a
SeoJaeHyuk <ssjjhhcc@gmail.com>
2023-05-24 13:22:09 +0900

    commit

--- thirdStudy/src/app/app.module.ts
+++ thirdStudy/src/app/app.module.ts
@@ -7,6 +7,22 @@ import { BoardComponent } from './board/board.component';
 import { AuthGuard } from './auth.guard';
 import { RouterModule,Routes} from '@angular/router'; //라우터
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+
+const fireEnvironment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2UDNn-LdW75DWZLwsM0yf99a7Dowq-Ag",
+    authDomain: "newmyproject-14680.firebaseapp.com",
+    projectId: "newmyproject-14680",
+    storageBucket: "newmyproject-14680.appspot.com",
+    messagingSenderId: "1007870971881",
+    appId: "1:1007870971881:web:01550b760bdd4ed91d8578",
+    measurementId: "G-9SDT9R9BJF"
+  }
+}
 const router : Routes = [  //라우팅
   {path : 'login' , component : LoginComponent},  
   {path : 'board' , component : BoardComponent, canActivate:[AuthGuard]},  //가드 추가
@@ -22,6 +38,8 @@ const router : Routes = [  //라우팅
     BrowserModule,
     RouterModule.forRoot(router,{enableTracing:false, useHash: true}),
     FormsModule,
+    AngularFireModule.initializeApp(fireEnvironment.firebase, '/'),   //파이어 베이스 모듈 사용
+    AngularFirestoreModule,  //파이어베이스 데이터베이스와 관련된 모듈 사용
   ],
   providers: [],
   bootstrap: [AppComponent]
