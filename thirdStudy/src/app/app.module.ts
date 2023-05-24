58667bf963e40ebf8418e11222d306b8543799eb
SeoJaeHyuk <ssjjhhcc@gmail.com>
2023-05-24 09:09:30 +0900

    commit

--- thirdStudy/src/app/app.module.ts
+++ thirdStudy/src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BoardComponent } from './board/board.component';
+
+import { RouterModule,Routes} from '@angular/router'; //라우터
+
+const router : Routes = [  //라우팅
+  {path : 'login' , component : LoginComponent},  
+  {path : 'board' , component : BoardComponent},
+  {path : '', redirectTo : '/login',  pathMatch : 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router,{enableTracing:false, useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
